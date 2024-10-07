047ef7622749d970b4b6a4a836acb5e29602593a
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:39:02 +0700

    Create perbandingan.dart

---
+++ perbandingan.dart
@@ -0,0 +1,12 @@
+//INI ADALAH OPERATOR PERBANDINGAN
+ void main(){
+  print(10 == 10);
+  print(10 != 10);
+  print(10 < 10);
+  print(10 > 10);
+  print(10 <= 10);
+  print(10 >= 10);
+
+   print('Rafiqi' == 'Rafiqi');
+   print('Rafiqi' != 'Rafiqi');
+ }
